29f61116411bad5c60cddb6ea9f0e2f226d28f1b
joajabn <jablonskaa.joanna@gmail.com>
2024-08-26 22:22:56 +0200

    update MergeSort and add MergeSortTest

--- src/main/java/pl/nyczjablonska/sorting/MergeSort.java
+++ src/main/java/pl/nyczjablonska/sorting/MergeSort.java
@@ -3,43 +3,44 @@ package pl.nyczjablonska.sorting;
 import java.util.Arrays;
 
 public class MergeSort {
-//    {10, 9, 3, 4, 11, 0, 5}
-    public static int[] sort(int[] array){
-        int middleIndex = (array.length - 1) / 2;
-        int[] leftArray = Arrays.copyOfRange(array, 0, middleIndex);
-        int[] rightArray = Arrays.copyOfRange(array, middleIndex, array.length);
-        if(leftArray.length > 1){
-            sort(leftArray);
+    //    {10, 9, 3, 4, 11, 0, 5}
+    public static int[] sort(int[] array) {
+//        int middleIndex = (array.length - 1) / 2;
+        int length = array.length;
+        int[] leftArray = Arrays.copyOfRange(array, 0, length / 2);
+        int[] rightArray = Arrays.copyOfRange(array, length / 2, array.length);
+        if (leftArray.length > 1) {
+            leftArray = sort(leftArray);
         }
 
-        if(rightArray.length > 1){
-            sort(rightArray);
+        if (rightArray.length > 1) {
+            rightArray = sort(rightArray);
         }
         return merge(leftArray, rightArray);
     }
 
-    public static int[] merge(int[] leftArray, int[] rightArray){
+    public static int[] merge(int[] leftArray, int[] rightArray) {
         int leftIndex = 0;
         int rightIndex = 0;
         int[] result = new int[leftArray.length + rightArray.length];
         int resultIndex = 0;
-        while(leftIndex < leftArray.length && rightIndex < rightArray.length){
-            if(leftArray[leftIndex] < rightArray[rightIndex]){
+        while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+            if (leftArray[leftIndex] < rightArray[rightIndex]) {
                 result[resultIndex] = leftArray[leftIndex];
                 resultIndex++;
                 leftIndex++;
             } else {
-                result[rightIndex] = rightArray[rightIndex];
+                result[resultIndex] = rightArray[rightIndex];
                 resultIndex++;
                 rightIndex++;
             }
         }
-        while(rightIndex < rightArray.length){
+        while (rightIndex < rightArray.length) {
             result[resultIndex] = rightArray[rightIndex];
             resultIndex++;
             rightIndex++;
         }
-        while(leftIndex < leftArray.length){
+        while (leftIndex < leftArray.length) {
             result[resultIndex] = leftArray[leftIndex];
             resultIndex++;
             leftIndex++;
