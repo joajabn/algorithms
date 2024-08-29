df86d4753fdea730ae0f2b25dac53c738f196aa8
joajabn <jablonskaa.joanna@gmail.com>
2024-08-29 11:42:22 +0200

    update Node and NodeTest (with null nodes and cycle)

--- src/main/java/pl/nyczjablonska/recursion/Node.java
+++ src/main/java/pl/nyczjablonska/recursion/Node.java
@@ -1,9 +1,12 @@
 package pl.nyczjablonska.recursion;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Node {
-    private Node leftNode;
-    private Node rightNode;
-    private int value;
+    private final Node leftNode;
+    private final Node rightNode;
+    private final int value;
 
     public Node(Node leftNode, Node rightNode, int value) {
         this.leftNode = leftNode;
@@ -12,11 +15,19 @@ public class Node {
     }
 
     public static void dfs(Node node){
+        Set<Node> nodeSet = new HashSet<>();
+        dfsRecursive(node, nodeSet);
+    }
+
+    private static void dfsRecursive(Node node, Set<Node> nodeSet) {
         if(node == null){
             return;
         }
-        System.out.println("Value -> " + node.value);
-        dfs(node.leftNode);
-        dfs(node.rightNode);
+        if (!nodeSet.contains(node)) {
+            nodeSet.add(node);
+            System.out.println("Value -> " + node.value);
+            dfsRecursive(node.leftNode, nodeSet);
+            dfsRecursive(node.rightNode, nodeSet);
+        }
     }
 }
--- src/test/java/pl/nyczjablonska/recursion/NodeTest.java
+++ src/test/java/pl/nyczjablonska/recursion/NodeTest.java
@@ -33,9 +33,11 @@ class NodeTest {
         Node.dfs(root);
 
         //expected output
-        String expectedOutput = "Value -> 1" + System.lineSeparator() +
-                "Value -> 2" + System.lineSeparator() +
-                "Value -> 3" + System.lineSeparator();
+        String expectedOutput = """
+                Value -> 1
+                Value -> 2
+                Value -> 3
+                """;
 
         // Verify output
         assertEquals(expectedOutput, outContent.toString());
@@ -58,19 +60,92 @@ class NodeTest {
         Node.dfs(root);
 
         //expected output
-        String expectedOutput = "Value -> 1" + System.lineSeparator() +
-                "Value -> 2" + System.lineSeparator() +
-                "Value -> 4" + System.lineSeparator() +
-                "Value -> 5" + System.lineSeparator() +
-                "Value -> 3" + System.lineSeparator() +
-                "Value -> 6" + System.lineSeparator() +
-                "Value -> 7" + System.lineSeparator();
+        String expectedOutput = """
+                Value -> 1
+                Value -> 2
+                Value -> 4
+                Value -> 5
+                Value -> 3
+                Value -> 6
+                Value -> 7
+                """;
 
         //then
         assertEquals(expectedOutput, outContent.toString());
+    }
+    @Test
+    public void testDfs6Nodes() {
+        //given
+        Node leftNode = new Node(
+                new Node(null, null, 4),
+                new Node(null, null, 5),
+                2);
+        Node rightNode = new Node
+                (new Node(null, null, 6), null, 3);
+        Node root = new Node(leftNode, rightNode, 1);
 
+        //when
+        Node.dfs(root);
+
+        //expected output
+        String expectedOutput = """
+                Value -> 1
+                Value -> 2
+                Value -> 4
+                Value -> 5
+                Value -> 3
+                Value -> 6
+                """;
+
+        //then
+        assertEquals(expectedOutput, outContent.toString());
     }
 
+    /**
+     *                  0
+     *               /     \
+     *             /        \
+     *           /           \
+     *         /              \
+     *       1   ___________>  2
+     *     /                 /  \
+     *    3                 5    6
+     *   / \               / \
+     *  7  8 <-------------   -- 9
+     *
+     *
+     **/
+    @Test
+    public void testDfsNodes() {
+        //given
+        var node9 = new Node(null, null, 9);
+        var node8 = new Node(null, null, 8);
+        var node7 = new Node(null, null, 7);
+        var node6 = new Node(null, null, 6);
+        var node5 = new Node(node8, node9, 5);
+        var node3 = new Node(node7, node8, 3);
+        var node2 = new Node(node5, node6, 2);
+        var node1 = new Node(node3, node2, 1);
+        Node root = new Node(node1, node2,  0);
 
+        //when
+        Node.dfs(root);
+
+        //excepted output
+        String expectedOutput = """
+                Value -> 0
+                Value -> 1
+                Value -> 3
+                Value -> 7
+                Value -> 8
+                Value -> 2
+                Value -> 5
+                Value -> 9
+                Value -> 6
+                """;
+
+        //then
+        assertEquals(expectedOutput, outContent.toString());
+    }
 
 }
