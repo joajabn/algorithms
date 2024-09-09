47ca5ebbbe38b99905e2d4d07ff17b52532dc11b
joajabn <jablonskaa.joanna@gmail.com>
2024-09-09 13:56:06 +0200

    replace unit tests with parametrised one

--- src/test/java/pl/nyczjablonska/recursion/BinarySearchTest.java
+++ src/test/java/pl/nyczjablonska/recursion/BinarySearchTest.java
@@ -1,77 +1,34 @@
 package pl.nyczjablonska.recursion;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTest {
     private final int[] sortedArray = {2, 3, 5, 8, 11, 15, 17, 25, 29, 34, 38};
-    private final int firstIndex = 0;
     private final int lastIndex = sortedArray.length - 1;
-    @Test
-    public void testBinarySearchRightExists(){
-        //given
-        int numberToFind = 34;
-
-        //when
-        int actual = BinarySearch.search(sortedArray, firstIndex, lastIndex, numberToFind);
-
-        //then
-        assertEquals(9, actual);
+    private static Stream<Arguments> provideNumbersAndResults() {
+        return Stream.of(
+                Arguments.of(34, 9),
+                Arguments.of(5, 2),
+                Arguments.of(15, 5),
+                Arguments.of(1, -1),
+                Arguments.of(40, -1),
+                Arguments.of(14, -1)
+        );
     }
-    @Test
-    public void testBinarySearchLeftExists(){
-        //given
-        int numberToFind = 5;
-
+    @ParameterizedTest
+    @MethodSource("provideNumbersAndResults")
+    public void testBinarySearch(int numberToFind, int expectedOutput){
         //when
+        int firstIndex = 0;
         int actual = BinarySearch.search(sortedArray, firstIndex, lastIndex, numberToFind);
 
         //then
-        assertEquals(2, actual);
-    }
-    @Test
-    public void testBinarySearchMiddleExists(){
-        //given
-        int numberToFind = 15;
-
-        //when
-        int actual = BinarySearch.search(sortedArray, firstIndex, lastIndex, numberToFind);
-
-        //then
-        assertEquals(5, actual);
-    }
-    @Test
-    public void testBinarySearchLeftDoesNotExist(){
-        //given
-        int numberToFind = 1;
-
-        //when
-        int actual = BinarySearch.search(sortedArray, firstIndex, lastIndex, numberToFind);
-
-        //then
-        assertEquals(-1, actual);
-    }
-    @Test
-    public void testBinarySearchRightDoesNotExist(){
-        //given
-        int numberToFind = 40;
-
-        //when
-        int actual = BinarySearch.search(sortedArray, firstIndex, lastIndex, numberToFind);
-
-        //then
-        assertEquals(-1, actual);
-    }
-    @Test
-    public void testBinarySearchMiddleDoesNotExist(){
-        //given
-        int numberToFind = 14;
-
-        //when
-        int actual = BinarySearch.search(sortedArray, firstIndex, lastIndex, numberToFind);
-
-        //then
-        assertEquals(-1, actual);
+        assertEquals(expectedOutput, actual);
     }
 }
