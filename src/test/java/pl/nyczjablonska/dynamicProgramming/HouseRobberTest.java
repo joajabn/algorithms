ccd8dac9420badbe8fde12e127400858d9d0e761
joajabn <jablonskaa.joanna@gmail.com>
2024-09-23 12:21:56 +0200

    add HouseRobber (only recursion part) and tests

---
+++ src/test/java/pl/nyczjablonska/dynamicProgramming/HouseRobberTest.java
@@ -0,0 +1,32 @@
+package pl.nyczjablonska.dynamicProgramming;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HouseRobberTest {
+
+    private static Stream<Arguments> provideArraysAndResults() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 1}, 4),
+                Arguments.of(new int[]{2, 7, 9, 3, 1}, 12),
+                Arguments.of(new int[]{10, 1, 10, 1, 10}, 30),
+                Arguments.of(new int[]{5}, 5),
+                Arguments.of(new int[]{10, 20}, 20),
+                Arguments.of(new int[]{5, 5, 5, 5, 5}, 15),
+                Arguments.of(new int[]{5, 3, 4, 11, 2}, 16),
+                Arguments.of(new int[]{}, 0)
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("provideArraysAndResults")
+    public void shouldReturnMaxAmountOfMoney(int[] array, int result){
+        int actual = HouseRobber.maxHouseRobbing(array);
+        assertEquals(result, actual);
+    }
+}
