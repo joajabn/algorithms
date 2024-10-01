c9f190727a8e91a91c84de67dbd0b455d6d7c0a4
joajabn <jablonskaa.joanna@gmail.com>
2024-10-01 09:23:10 +0200

    add new solution for memoRobbing

--- src/main/java/pl/nyczjablonska/dynamicProgramming/HouseRobber.java
+++ src/main/java/pl/nyczjablonska/dynamicProgramming/HouseRobber.java
@@ -6,36 +6,51 @@ public class HouseRobber {
     public static int maxHouseRobbing(int[] array) {
         int[] dp = new int[array.length];
         Arrays.fill(dp, -1);
-        return memoRobHouse(array, 0, array.length - 1, dp);
+        return memoRobHouse(array, array.length - 1, dp);
     }
 
     public static int robHouse(int[] array, int cost, int index) {
         if (index >= array.length) {
             return cost;
         }
-        int take = robHouse(array, cost + array[index], index + 2);
+        int take = cost + robHouse(array, cost + array[index], index + 2);
         int notTake = robHouse(array, cost, index + 1);
         return Math.max(take, notTake);
     }
 
-    public static int memoRobHouse(int[] array, int cost, int index, int[] dp) {
-        if(index == 0 || index == 1){
-            dp[index] = cost;
-            return dp[index];
+    public static int memoRobHouse(int[] array, int index, int[] dp) {
+//        if(index == 0 || index == 1){
+//            dp[index] = cost;
+//            return dp[index];
+//        }
+//        int take = 0;
+//        int notTake = 0;
+//        if(dp[index - 2] != -1){
+//            take = dp[index - 2];
+//        } else {
+//            take = array[index] + memoRobHouse(array, cost, index - 2, dp);
+//        }
+//        if(dp[index - 1] != -1){
+//            notTake = dp[index - 1];
+//        } else {
+//            notTake = memoRobHouse(array, cost, index - 1, dp);
+//        }
+//        dp[index] = Math.max(take, notTake);
+//        return dp[index];
+//    }
+        if (index < 0) {
+            return 0; // Base case: no more houses to rob
         }
-        int take = 0;
-        int notTake = 0;
-        if(dp[index - 2] != -1){
-            take = dp[index - 2];
-        } else {
-            take = memoRobHouse(array, cost + array[index], index - 2, dp);
+
+        if (dp[index] != -1) {
+            return dp[index]; // Return already computed result
         }
-        if(dp[index - 1] != -1){
-            notTake = dp[index - 1];
-        } else {
-            notTake = memoRobHouse(array, cost, index - 1, dp);
-        }
-        dp[index] = Math.max(take, notTake);
+
+        // Recurrence relation: max of robbing current house or skipping it
+        int take = array[index] + memoRobHouse(array, index - 2, dp); // Rob current house
+        int notTake = memoRobHouse(array, index - 1, dp); // Skip current house
+
+        dp[index] = Math.max(take, notTake); // Store result in dp array
         return dp[index];
     }
 }
