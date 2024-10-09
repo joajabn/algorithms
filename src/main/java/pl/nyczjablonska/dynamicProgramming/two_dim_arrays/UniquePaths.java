51453f1656fdd279542b15092d3a18af05f53880
joajabn <jablonskaa.joanna@gmail.com>
2024-10-09 10:56:07 +0200

    add method which uses memoization

--- src/main/java/pl/nyczjablonska/dynamicProgramming/two_dim_arrays/UniquePaths.java
+++ src/main/java/pl/nyczjablonska/dynamicProgramming/two_dim_arrays/UniquePaths.java
@@ -1,8 +1,14 @@
 package pl.nyczjablonska.dynamicProgramming.two_dim_arrays;
 
+import java.util.Arrays;
+
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
-        return countUniquePathsRec(m - 1, n - 1);
+        int[][] t = new int[m][n];
+        for(int[] row : t){
+            Arrays.fill(row, -1);
+        }
+        return countUniquePathsMemo(m - 1, n - 1, t);
     }
 
     private int countUniquePathsRec(int m, int n) {
@@ -15,12 +21,20 @@ public class UniquePaths {
         return countUniquePathsRec(m - 1, n) + countUniquePathsRec(m, n - 1);
     }
 
-    private int countUniquePathsMemo(int m, int n, int[] t) {
+    private int countUniquePathsMemo(int m, int n, int[][] t) {
         if(m == 0 && n == 0) {
             return 1;
         }
         if(m < 0 || n < 0){
             return 0;
+        }
+        int left = t[m][n - 1];
+        int top = t[m - 1][n];
+        if(left != -1){
+            return t[m][n-1];
+        }
+        if(top != -1){
+            return t[m - 1][n];
         }
         return countUniquePathsRec(m - 1, n) + countUniquePathsRec(m, n - 1);
     }
