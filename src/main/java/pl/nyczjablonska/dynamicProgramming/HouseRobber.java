605e2aebd3f04d881b1ff79becf642d69a233491
joajabn <jablonskaa.joanna@gmail.com>
2024-10-16 17:26:02 +0200

    remove unnecessary comments

--- src/main/java/pl/nyczjablonska/dynamicProgramming/HouseRobber.java
+++ src/main/java/pl/nyczjablonska/dynamicProgramming/HouseRobber.java
@@ -6,7 +6,6 @@ public class HouseRobber {
     public static int maxHouseRobbing(int[] array) {
         int[] dp = new int[array.length];
         Arrays.fill(dp, -1);
-//        return memoRobHouse(array, array.length - 1, dp);
         return dpRobHouse(array);
     }
 
@@ -20,25 +19,6 @@ public class HouseRobber {
     }
 
     public static int memoRobHouse(int[] array, int index, int[] dp) {
-//        if(index == 0 || index == 1){
-//            dp[index] = cost;
-//            return dp[index];
-//        }
-//        int take = 0;
-//        int notTake = 0;
-//        if(dp[index - 2] != -1){
-//            take = dp[index - 2];
-//        } else {
-//            take = array[index] + memoRobHouse(array, cost, index - 2, dp);
-//        }
-//        if(dp[index - 1] != -1){
-//            notTake = dp[index - 1];
-//        } else {
-//            notTake = memoRobHouse(array, cost, index - 1, dp);
-//        }
-//        dp[index] = Math.max(take, notTake);
-//        return dp[index];
-//    }
         if (index < 0) {
             return 0; // Base case: no more houses to rob
         }
